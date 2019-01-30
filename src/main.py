1231e256f75a3e70e7012004704339da7bac419d
Ross Dyson <dyson.ross@gmail.com>
2019-01-30 22:08:44 +0900

    add new routes

--- src/main.py
+++ src/main.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/control/start')
+def run_start():
+    return 'start'
+
+@app.route('/control/stop')
+def run_stop():
+    return 'stop'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
